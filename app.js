16463550a99e3f9f1149f9217e033cf6f8fd261f
Captain <furlenco@Furlencos-MacBook-Pro.local>
2020-10-02 16:09:43 +0530

    mongoose and dotenv added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config')
 
 //ROUTES
 app.get('/',(req,res)=>{
@@ -10,5 +12,13 @@ app.get('/posts',(req,res)=>{
     res.send("We are on posts");
 });
 
+
+
+//Connect to db
+mongoose.connect(process.env.DB_CONNECTION,
+{ useNewUrlParser: true },
+()=> console.log('connected to db'),
+()=>console.log(require('dotenv').config())
+)
 //How do we start listening to the server
 app.listen(3000);
