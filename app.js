5752c4f5e5d60157651b0480bf65e2bf5021b8be
Captain <furlenco@Furlencos-MacBook-Pro.local>
2020-10-02 16:24:02 +0530

    routes added

--- app.js
+++ app.js
@@ -3,22 +3,19 @@ const app = express();
 const mongoose = require('mongoose');
 require('dotenv/config')
 
+//import Routes
+const postsRoute = require('./routes/posts')
+
+app.use('/posts' , postsRoute)
+
 //ROUTES
 app.get('/',(req,res)=>{
     res.send("We are on home");
 });
 
-app.get('/posts',(req,res)=>{
-    res.send("We are on posts");
-});
-
-
-
 //Connect to db
 mongoose.connect(process.env.DB_CONNECTION,
 { useNewUrlParser: true },
-()=> console.log('connected to db'),
-()=>console.log(require('dotenv').config())
-)
+()=> console.log('connected to db') )
 //How do we start listening to the server
 app.listen(3000);
